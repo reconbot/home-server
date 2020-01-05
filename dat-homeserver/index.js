64f565613959d03b1e982fb431458adaaab4f081
Francis Gulotta <wizard@roborooter.com>
2020-01-04 21:15:41 -0500

    ENV based dat-librarian

--- dat-homeserver/index.js
+++ dat-homeserver/index.js
@@ -1,3 +1,43 @@
-const reconbot = require('reconbot')
+const DatLibrarian = require('dat-librarian')
 
-setInterval(() => console.log(reconbot, new Date()), 1000)
+const dataDirectory = process.env.DATA_DIRECTORY
+if (!dataDirectory) {
+  throw new Error('DATA_DIRECTORY not set')
+}
+
+const archivesString = process.env.ARCHIVES
+if (!archivesString) {
+  throw new Error('ARCHIVES not set')
+}
+
+process.env.log = 'true'
+
+const datId = dat => dat.archive.key.toString('hex')
+const subscribeStats = dat => {
+  const stats = dat.trackStats()
+  setInterval(() => {
+    const { network, peers } = stats
+    console.log(JSON.stringify({ id: datId(dat), network, peers }))
+  }, 5000)
+}
+
+async function run() {
+  const librarian = new DatLibrarian({ dir: dataDirectory })
+  librarian.on('add', dat => {
+    console.log('add', datId(dat))
+    subscribeStats(dat)
+  })
+  librarian.on('join', dat => console.log('join', datId(dat)))
+  librarian.on('remove', dat => console.log('remove', datId(dat)))
+
+  await librarian.load()
+  const archives = archivesString.split(',')
+  for (const archive of archives) {
+    await librarian.add(archive)
+  }
+}
+
+run().catch(err => {
+  console.error(err)
+  process.exit(1)
+})
